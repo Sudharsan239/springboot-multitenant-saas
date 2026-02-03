9357b30bc1d86b81e4d41a0af4682e6570b0172b
Sudharsan <sudharsan23.ramesh@gmail.com>
2026-02-03 23:39:13 +0530

    initial commit

---
+++ src/main/java/com/orgflow/springboot_multitenant_saas/SpringbootMultitenantSaasApplication.java
@@ -0,0 +1,13 @@
+package com.orgflow.springboot_multitenant_saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultitenantSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMultitenantSaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orgflow/springboot_multitenant_saas/SpringbootMultitenantSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orgflow.springboot_multitenant_saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultitenantSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
